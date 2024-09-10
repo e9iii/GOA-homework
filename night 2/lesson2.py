ced24681d3ad5153c7efa62916e9879697a7bfbb
e9iii <astralien@bk.ru>
2024-09-10 17:50:34 +0400

    first commit

---
+++ night 2/lesson2.py
@@ -0,0 +1,53 @@
+#comm = "omari"
+#com = "abubakarovi"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჯებელი სიმბოლო
+# "omari" არის ცვლადისტვის მნიშვნელობა
+
+
+#print(comm)
+#print(com)
+#პრინტ გუნქციას გადაეცემა ეკრანზე გამოსტანი ობიექტი
+
+#com = "omari" #ეს არის str (string) ტიპის ცვლადი
+#age = 22 # ეს არის int (integer) მთელი რიცხვი
+#height = 184.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#booLean (bool) ტიპის ცვლადი
+
+#knows_programming = True #True an FaLse
+#is_ugly = False  #snakecase(უბრალო წერის სტილი სტანდარტულად)
+
+#sUgly = False # ჯავასკრიპტუი cameLcase
+
+
+#print(comm + " " + com)
+
+#სტრინგი არის ბრჭყალეში მოქცეული სიმბოლოები
+#print(comm + height)
+ 
+#print(type(age))
+#print(type(comm))
+#print(type(com))
+#print(type(height))
+#print(type(knows_programming))
+
+#print(comm +  " " + str(age))
+
+
+co = "my"
+com = "name"
+comm = "umar"
+cim = "I"
+cem = "love"
+cmm = "minecraft"
+cm = "age"
+cc = "is"
+c = "am"
+cmmm = "15"
+ccc = "years"
+come = "old"
+comen = "height"
+coment = "174+"
+cament = "centimeters"
+
+print(co + " " + str(com) + " " + str(cc) + " " + str(comm) + " " + str(cim) + " " + str(cem) + " " + str(cmm) + " " + str(cim) + " " + str(c) + " " + str(cmmm) + " " + str(ccc) + " " + str(come) + " " + str(comen) + " " + str(coment) + " " + str(cament))
