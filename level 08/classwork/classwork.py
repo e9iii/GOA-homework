fac503d06256f07424ef12d0b1d4f5e67b54d49b
e9iii <astralien@bk.ru>
2024-11-19 22:23:45 +0400

    commit

---
+++ level 08/classwork/classwork.py
@@ -0,0 +1,19 @@
+# 1) comparators operatorors 2-2 მაგალითი
+# #  >
+# #  <
+# #  >=
+# #  <=
+# #  ==
+# #  !=
+
+# # ✅
+print(1 > 2)
+print(1 < 2)
+print(9 >= 9)
+print(9 >= 8)
+print(10 <= 10)
+print(10 <= 256)
+print(20 == 20)
+print(22 == 1)
+print(15 != 15)
+print(15 != 2)
